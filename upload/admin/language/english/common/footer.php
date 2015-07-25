22a2749fe44fc27cf994a2fb38903b20a772945c
amantran <amantran@forestallers.in>
2015-07-25 23:58:32 +0530

    first commit

---
+++ upload/admin/language/english/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer'] 	= '<a target="_blank" href="http://www.forestallers.in">Innovateur by Forestallers</a> &copy; 2014-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] 	= 'Version %s';
